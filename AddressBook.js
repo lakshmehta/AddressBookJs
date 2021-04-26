6ac6248c6e8584377f5d196e21ae6cca24637435
unknown <lakshmehta06@gmail.com>
2021-04-26 10:35:06 +0530

    [LAKSH ADDED DUPLICATE ENTRY]

--- AddressBook.js
+++ AddressBook.js
@@ -185,3 +185,14 @@ function numberOfContact() {
 }
 
 numberOfContact();
+function duplicateEntry() {
+    let userInput = prompt("Enter name to check Entry in addressbook : ");
+    addressBookArray.forEach(addressBook => {
+        if(addressBook.firstName == userInput) {
+            console.log("Contact already exists in the book");
+        } else {
+            console.log("Contact not exists in the book");
+        }
+});
+}
+duplicateEntry();
