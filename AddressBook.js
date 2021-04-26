058ee10fecbc505e0a5a66526d2cb2aa096cf9a6
unknown <lakshmehta06@gmail.com>
2021-04-26 10:49:25 +0530

    [LAKSH ADDED SEARCH FUNCTION]

--- AddressBook.js
+++ AddressBook.js
@@ -224,3 +224,13 @@ function viewByCityOrState() {
 }
 
 viewByCityOrState();
+
+
+function countByCity(city) {
+    return addressBookArray.filter(contact => contact.city == city).reduce((count, contact) => count += 1, 0);
+}
+function countByState(state) {
+    return addressBookArray.filter(contact => contact.state == state).reduce((count, contact) => count += 1, 0)
+}
+console.log("number of Person in city " +countByCity("Virar"));
+console.log("number of Person in state " +countByState("Maharashtra"));
