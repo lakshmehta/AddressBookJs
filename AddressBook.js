404ca4c984430ab663a134c2819c4d137f0fcc88
unknown <lakshmehta06@gmail.com>
2021-04-26 10:27:03 +0530

    [LAKSH ADDED DELETE CONTACT METHOD]

--- AddressBook.js
+++ AddressBook.js
@@ -163,3 +163,18 @@ addressBookArray.forEach(addressBook => {
 }
 
 editPerson();
+
+
+
+function deleteContact() {
+    let userInput = prompt("Enter name to Delete Entry : ");
+    addressBookArray.forEach(addressBook => {
+        if(addressBook.firstName == userInput) {
+            addressBookArray.splice(addressBookArray.indexOf(addressBook), 1)
+            console.log("Record Deleted");
+            console.log(addressBookArray);
+        }
+    });
+}
+
+deleteContact();
