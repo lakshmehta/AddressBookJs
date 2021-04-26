95dcca9ab4df38c10bdf2c56ffc11db84c93d371
unknown <lakshmehta06@gmail.com>
2021-04-26 10:23:31 +0530

    [LAKSH ADDED FIND EXIST CONTACT METHOD]

--- AddressBook.js
+++ AddressBook.js
@@ -122,3 +122,44 @@ let addressBookArray= new Array();
 addressBookArray.push(addressBook1);
 addressBookArray.push(addressBook2);
 console.log("\nNew Contact Added to Array " +addressBookArray);
+
+const prompt = require('prompt-sync')({sigint: true});
+
+function editPerson() {
+let userInput = prompt("Enter name to update Contact : ");
+
+addressBookArray.forEach(addressBook => {
+    if(addressBook.firstName == userInput) {
+    console.log("1.PhoneNumber" + "\n2.Address" + "\n3.quit");
+    var choice = prompt("Select any option to edit :")
+
+    switch(choice) {
+        case "1" :
+            phoneNumber = prompt("Enter new Phone Number :");
+            addressBook.phoneNumber = phoneNumber;
+            console.log(addressBook.toString());
+            console.log("Phone number updated")
+        
+            break;
+        case "2" :
+            city = prompt("Enter new city : ");
+            addressBook.city = city;
+            console.log(addressBook.toString());
+
+            state = prompt("Enter new state : ");
+            addressBook.state = state;
+            console.log(addressBook.toString());
+
+            zip = prompt("Enter new zip : ");
+            addressBook.zip = zip;
+            console.log(addressBook.toString());
+            break;
+        default :
+            console.log("Incorrect choice");
+            break;
+    }  
+}
+}); 
+}
+
+editPerson();
